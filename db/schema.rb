0449db2e7d68659babe3a342776bef23e35c37de
SamArsanios <samson.kibrom@hotmail.com>
2021-02-08 17:55:35 +0300

    Add comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_141754) do
+ActiveRecord::Schema.define(version: 2021_02_08_145457) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.text "email"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -29,5 +39,6 @@ ActiveRecord::Schema.define(version: 2021_02_08_141754) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
