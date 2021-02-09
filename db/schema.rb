4dd8323b021c888f6e25b94766427118b8b11152
SamArsanios <samson.kibrom@hotmail.com>
2021-02-09 09:42:07 +0300

    Remove email column from table comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_09_063751) do
+ActiveRecord::Schema.define(version: 2021_02_09_064118) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
-    t.text "email"
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
