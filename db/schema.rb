65891758a274fab7e1b1fab6fe73a1424e663941
SamArsanios <samson.kibrom@hotmail.com>
2021-02-09 09:40:22 +0300

    Remove commenter column from table comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_160528) do
+ActiveRecord::Schema.define(version: 2021_02_09_063751) do
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
     t.text "body"
     t.text "email"
     t.integer "post_id", null: false
